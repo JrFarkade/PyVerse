1a5c68c732beab360235eacf78da56b163f3e4fd
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-11 22:01:49 +0530

    Create 11a.py

---
+++ 01Jan/11a.py
@@ -0,0 +1,23 @@
+# Phone Number to Words Converter
+
+phone_num = input("Enter Your Phone Number: ")
+
+digit_mapping = {
+    "0": "zero",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine"
+}
+
+output = ""
+
+for ch in phone_num:
+    output += digit_mapping.get(ch, "Invalid") + " "
+
+print(output.strip())
