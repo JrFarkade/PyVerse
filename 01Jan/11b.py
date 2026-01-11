b959bfe8061ddbbe13c370e10674481f0110c65a
Sahil Farkade <121764039+JrFarkade@users.noreply.github.com>
2026-01-11 22:07:06 +0530

    Straw Hat Crew Dictionary

---
+++ 01Jan/11b.py
@@ -0,0 +1,21 @@
+# Straw Hat Crew Dictionary (Python)
+
+straw_hat_crew = {
+    "Captain": "Monkey D. Luffy",
+    "Swordsman": "Roronoa Zoro",
+    "Navigator": "Nami",
+    "Sniper": "Usopp",
+    "Cook": "Sanji",
+    "Doctor": "Tony Tony Chopper",
+    "Archaeologist": "Nico Robin",
+    "Shipwright": "Franky",
+    "Musician": "Brook",
+    "Helmsman": "Jinbe"
+}
+
+# Print the Captain
+print("Captain:", straw_hat_crew["Captain"])
+
+# Print full dictionary
+print("\nFull Straw Hat Crew:")
+print(straw_hat_crew)
